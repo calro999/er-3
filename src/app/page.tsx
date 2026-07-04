4912fe39e7ea1034409a4bebf305dc47a3b02f43
calro999 <gamingmedia.mail@gmail.com>
2026-07-04 13:08:34 +0900

    feat: generate and add new high quality killer posts and fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { Metadata } from "next";
+import Link from "next/link";
 import PostListContainer from "./components/PostListContainer";
 
 interface Post {
