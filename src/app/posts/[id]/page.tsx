5f34260353d817b29bc8d0b0d86ae1f3a4582f07
calro999 <gamingmedia.mail@gmail.com>
2026-07-15 16:58:04 +0900

    feat: add shinobi admax script to post pages

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import Link from "next/link";
+import Script from "next/script";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import { censorText } from "@/lib/censor";
@@ -444,6 +445,14 @@ export default async function PostPage({ params }: { params: Promise<{ id: strin
             </p>
           </section>
 
+          {/* Admax Advertisement */}
+          <div className="pt-8 text-center flex justify-center">
+            <Script
+              async
+              src="https://adm.shinobi.jp/st/auto.js"
+              data-admax-id="6940cf426d8b05585fbd28930455285d"
+            />
+          </div>
         </div>
       </article>
     </>
